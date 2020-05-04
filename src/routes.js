b2149b0afc1facf4a0326ca1ec18c980094e0e6f
aditi <adtsolanki22@gmail.com>
2020-05-04 16:32:16 +0530

    Added Register Route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
-//import { getUrl as getRoutePath } from './helpers/url-manager';
+import { getUrl as getRoutePath } from './helpers/url-manager';
 import RouterLoader from './components/router-loader';
-//import './register-routes';
+import './register-routes';
 
 //file-2
 export default (hostName, pathName, cookie) => {
@@ -11,8 +11,8 @@ export default (hostName, pathName, cookie) => {
     <Route>
       <Route path="/">
         <Route
-          // path={getRoutePath('landing')}
-          path="/landing"
+          path={getRoutePath('landing')}
+          //path="/landing"
           component={RouterLoader({
             loader: () => import('./containers/landing')
           })}
