637a8dfc36fc04cee7de1f3461fd4ca9da63c2a9
Unknown <adtsolanki22@gmail.com>
2020-05-10 01:10:29 +0530

    Added Slideshow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './containers/Navbar/index'
 import Header from './containers/Header/index'
+import Contentdata from './containers/Contentdata/index'
 class App extends React.Component {
   render() {
   return (
@@ -10,6 +11,9 @@ class App extends React.Component {
       </header>
       <div className="navbar">
         <Navbar /> 
+      </div>
+      <div className= "contentdata">
+        <Contentdata />
       </div>
     </div>
   )};
---
+++ src/containers/Contentdata/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+class Contentdata extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="box">
+                </div>
+            </div>
+        );
+    }
+}
+export default Contentdata;
