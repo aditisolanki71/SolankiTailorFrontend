20de68968cc2bafc50868a601ee4bd9f899f32f4
Unknown <adtsolanki22@gmail.com>
2020-05-10 14:27:47 +0530

    Added Key Feature

--- src/containers/Contentdata/index.js
+++ src/containers/Contentdata/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Banner from './Banner/index'
 import './index.css'
 class Contentdata extends React.Component {
     render() {
         return (
             <div>
-                <div className="box">
+                <div className="row">
+                    <div className="box">
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="banner">
+                        <Banner />
+                    </div>
                 </div>
             </div>
         );
