e3a84d1b48a6c8554d3ae1767c2740bfe9f278f3
Unknown <adtsolanki22@gmail.com>
2020-05-14 14:24:04 +0530

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './containers/Homepage/Header/index'
+import SidebarPage from './containers/Homepage/Sidebar/index'
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
 class App extends React.Component {
   render() {
   return (
     <div>
         <Header />
+        <SidebarPage />
       </div>
    )};
 }
