a919819b7b4cf1d8e3f87ee84ef133642a63f828
Unknown <adtsolanki22@gmail.com>
2020-06-13 23:44:08 +0530

    Banner design

--- src/containers/Dashboard/banner.js
+++ src/containers/Dashboard/banner.js
@@ -4,13 +4,23 @@ import { Grid, Image } from "semantic-ui-react";
 class Banner extends Component {
   render() {
     const { list } = this.props;
+    let columnLength = list.length;
+    if (columnLength > 3) {
+      columnLength = 3;
+    }
     return (
       <Grid>
-        <Grid.Row>
-          <Grid.Column width={2}></Grid.Column>
+        <Grid.Row columns={columnLength} style={{ margin: "50px" }}>
           {(list || []).length > 0
             ? list.map((item, index) => (
-                <Grid.Column key={index} width={4} style={{ padding: "20px" }}>
+                <Grid.Column
+                  key={index}
+                  style={{
+                    padding: "10px",
+                    "background-color": "#ffffff",
+                    margin: "20px 0px",
+                  }}
+                >
                   <p style={{ textAlign: "center" }}>{item.name}</p>
                   <Image
                     src={`/file/download/${item.image}`}
@@ -18,11 +28,11 @@ class Banner extends Component {
                     height={"300px"}
                     width={"350px"}
                     style={{ borderRadius: "10px" }}
+                    floated="left"
                   />
                 </Grid.Column>
               ))
             : "no data found"}
-          <Grid.Column width={2}></Grid.Column>
         </Grid.Row>
       </Grid>
     );
