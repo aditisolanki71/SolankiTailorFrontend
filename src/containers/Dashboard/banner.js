aab3a16f6ff70b027f1999cc203134d3c9ba9910
Unknown <adtsolanki22@gmail.com>
2020-06-13 15:14:51 +0530

    Main Category(Banner) image upload

--- src/containers/Dashboard/banner.js
+++ src/containers/Dashboard/banner.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Image } from "semantic-ui-react";
 
-import img1 from "./../../images/img1.jpg";
-import img2 from "./../../images/img2.jpg";
-import img4 from "./../../images/img4.jpg";
-
 class Banner extends Component {
   render() {
     const { list } = this.props;
@@ -17,7 +13,8 @@ class Banner extends Component {
                 <Grid.Column key={index} width={4} style={{ padding: "20px" }}>
                   <p style={{ textAlign: "center" }}>{item.name}</p>
                   <Image
-                    src={img2}
+                    src={`/file/download/${item.image}`}
+                    alt={item.image}
                     height={"300px"}
                     width={"350px"}
                     style={{ borderRadius: "10px" }}
