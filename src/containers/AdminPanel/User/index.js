fe558576f0402d0f88e6dbe19799f90e18fdfb86
Unknown <adtsolanki22@gmail.com>
2020-06-21 18:42:27 +0530

    User list added

--- src/containers/AdminPanel/User/index.js
+++ src/containers/AdminPanel/User/index.js
@@ -1,14 +1,18 @@
 import React, { Component, Fragment } from "react";
 import { Header } from "semantic-ui-react";
-import { listApi as allUser } from "../../../redux/modules/user";
+import { listApi as allUser, readApi } from "../../../redux/modules/user";
 import HeaderPage from "../../Dashboard/header";
 import Footer from "../../Dashboard/footer";
 import UserList from "./user-list";
-class BlogPage extends Component {
+import CreateModal from "./create-modal";
+class UserPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      formdata: {},
       userlist: [],
+      modalOpen: false,
+      readOnly: false,
     };
   }
   componentWillMount() {
@@ -19,8 +23,41 @@ class BlogPage extends Component {
       this.setState({ userlist: data });
     });
   }
+
+  closeModal = () =>
+    this.setState({
+      formdata: {},
+      modalOpen: false,
+    });
+
+  handleSubmit = (requestData, id) => {
+    console.log("inside handle submit");
+    // this.createOrUpdateUser(requestData, id).then(() => {
+    //   this.closeModal();
+    //   window.__SCHEDULER__.rescheduleAction("fetchTechnicians");
+    // });
+  };
+  setEditModal = (id) => {
+    if (id) {
+      readApi(id).then((data) => {
+        this.setState({
+          formdata: { ...data },
+          readOnly: true,
+          modalOpen: true,
+        });
+      });
+    } else {
+      this.setState({
+        formdata: {},
+        readOnly: false,
+        modalOpen: true,
+      });
+    }
+  };
+
   render() {
     const { userlist } = this.state;
+    console.log("user list is", userlist);
     return (
       <Fragment>
         <HeaderPage>
@@ -28,15 +65,23 @@ class BlogPage extends Component {
             Users
           </Header>
           <UserList
-            list={this.state.userlist}
+            list={userlist}
             onEdit={this.setEditModal}
             onRemove={this.removeUser}
             // me={this.props.me}
           />
+          {this.state.modalOpen && (
+            <CreateModal
+              onCreate={this.handleSubmit}
+              onClose={this.closeModal}
+              formdata={this.state.formdata}
+              readOnly={this.state.readOnly}
+            />
+          )}
         </HeaderPage>
         <Footer />
       </Fragment>
     );
   }
 }
-export default BlogPage;
+export default UserPage;
--- src/containers/AdminPanel/User/user-list.js
+++ src/containers/AdminPanel/User/user-list.js
@@ -1,15 +1,89 @@
 //file-3
-import React, { Fragment } from "react";
-class UserList extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <h1>user ist page</h1>
-        </div>
-      </Fragment>
-    );
-  }
-}
+// import React, { Fragment } from "react";
+// class UserList extends React.Component {
+//   render() {
+//     return (
+//       <Fragment>
+//         <div>
+//           <h1>user ist page</h1>
+//         </div>
+//       </Fragment>
+//     );
+//   }
+// }
+
+// export default UserList;
+
+/*eslint-disable */
+import React from "react";
+import { Table, Dropdown } from "semantic-ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const wordWrap = {
+  maxWidth: 0,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap"
+};
+
+const getHeaderRow = () => (
+  <Table.Row>
+    <Table.HeaderCell>Name</Table.HeaderCell>
+    <Table.HeaderCell>Email</Table.HeaderCell>
+    <Table.HeaderCell>Address</Table.HeaderCell>
+    <Table.HeaderCell>Action</Table.HeaderCell>
+  </Table.Row>
+);
+
+const buildTableRow = (item, props) => (
+  <Table.Row
+    className="pointer"
+    key={item.id}
+    onClick={() => props.onEdit(item.id)}
+  >
+    <Table.Cell style={wordWrap}>{item.name}</Table.Cell>
+    <Table.Cell style={wordWrap}>{item.email}</Table.Cell>
+    <Table.Cell style={wordWrap}>{item.address}</Table.Cell>
+    <Table.Cell style={{ overflow: "unset" }}>
+     <i
+        onClick={e => {
+          e.stopPropagation();
+          props.onEdit(item.id);
+        }}
+        className="edit icon"
+      />
+      <i
+        onClick={e => {
+          e.stopPropagation();
+          if (!item.oob) props.onRemove(item.id);
+        }}
+        // onClick={(e) => { e.stopPropagation(); props.onRemove(item.id); }}
+        className={`trash icon ${item.oob ? "disabled no-pointer" : ""}`}
+      />
+    </Table.Cell>
+  </Table.Row>
+);
+
+export const UserList = props => (
+  <Table celled>
+    <Table.Header>{getHeaderRow()}</Table.Header>
+    <Table.Body>
+      {(props.list || []).length > 0 ? (
+        props.list.map((item, index) => buildTableRow(item, props, index))
+      ) : (
+        <Table.Row>
+          <Table.Cell colSpan={4} className="no-data-found">
+            NO Data Found
+          </Table.Cell>
+        </Table.Row>
+      )}
+    </Table.Body>
+  </Table>
+);
+
+UserList.defaultProps = {
+  list: [],
+  onEdit: e => e,
+  onRemove: e => e
+};
 
 export default UserList;
