ecf973159db7e40099a3feb8f17dfe2baab2cc25
Unknown <adtsolanki22@gmail.com>
2020-06-25 19:50:01 +0530

    User api

--- src/redux/modules/user/api.js
+++ src/redux/modules/user/api.js
@@ -4,3 +4,28 @@ export function listApi() {
     return response;
   });
 }
+export function readApi(id) {
+  return window.__API__.get(`${urls.ALLUSER}/${id}`);
+}
+
+export function removeApi(id) {
+  return window.__API__.del(`${urls.ALLUSER}/${id}`, {
+    message: "user has been deleted successfully.",
+  });
+}
+
+export function updateApi(data, id) {
+  console.log("user update api", data);
+  console.log("id is", id);
+  return window.__API__.post(`${urls.ALLUSER}/${id}`, {
+    data,
+    message: "User has been updated successfully",
+  });
+}
+
+export function createApi(data) {
+  return window.__API__.post(urls.ALLUSER, {
+    data,
+    message: "User has been created successfully.",
+  });
+}
